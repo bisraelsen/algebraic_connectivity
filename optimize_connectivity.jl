2097b6fa0c99a283025110b24555e61a3c9818d2
Brett Israelsen <brett.israelsen@gmail.com>
2016-03-31 14:23:20 -0600

    modified probabiliyt schedule, added auto folder name generation, and progress bar

--- optimize_connectivity.jl
+++ optimize_connectivity.jl
@@ -3,12 +3,14 @@ include("utilities.jl")
 include("algConnectivity.jl")
 include("arg_config.jl")
 using PyPlot
+using ProgressMeter
 
 function Paccept(c,c_prime,T,its)
     if c_prime >= c
         return 1
     else
-        return exp(-its)/exp(-1)
+        normalizedP = exp(-its)/exp(-1)
+        return normalizedP/(c-c_prime)
     end
 end
 
@@ -16,42 +18,48 @@ function main()
     args = parse_commandline()
     n = args["vertices"]
     e = args["edges"]
+    if args["out_folder"] == ""
+        dt = replace(string(now()),"T","_")
+        dt = replace(dt,":","_")
+        dt = replace(dt,"-","_")
+        out_fldr = string("res_",dt)
+    else
+        out_fldr = args["out_folder"]
+    end
 
     if args["simple_graph"]
-        args["out_folder"] = string(args["out_folder"], "_sg")
+        out_fldr = string(out_fldr, "_sg")
         g = simple_NE_sgraph(n,e)
     else
         g = simple_NE_graph(n,e)
     end
 
     if !args["no_graph_dot"]
-        write_to_file(g,args["out_folder"],args["init_fname"])
+        write_to_file(g,out_fldr,args["init_fname"])
     end
     if args["plot_graphs"]
         plot(g)
     end
     if args["save_graphs_plot"]
-        plot_to_file(g,args["out_folder"],args["init_fname"])
+        plot_to_file(g,out_fldr,args["init_fname"])
     end
 
     current_state = deepcopy(g)
     tot_its = args["iterations"]
     edge_change_prob = args["edge_rewire"]
     T = args["temperature"]
-    c = AlgConnectivity(current_state)
+    c = AlgConnectivity(current_state,args["sparse_rep"])
     @printf("Original connectivity: %.3f\n",c)
     c_hist = zeros(tot_its,1)
+    p = Progress(tot_its, 30, "Progress: ", 50)
     for i = 1:tot_its
+        update!(p,i)
         c_hist[i] = c
-        if mod(i,10000) == 0
-            @printf("iteration %d\n",i)
-        end
         candidate = rewire(deepcopy(current_state),edge_change_prob)
-        c_prime = AlgConnectivity(candidate)
-            # @printf("Paccept: %0.3f\n",Paccept(c,c_prime,T))
+        c_prime = AlgConnectivity(candidate,args["sparse_rep"])
         accept_p = Paccept(c,c_prime,T,tot_its)
         if rand() < accept_p
-            # better, lets move there
+            # Moving here
             c = c_prime
             current_state = deepcopy(candidate)
         else
@@ -60,18 +68,18 @@ function main()
     end
     @printf("Final connectivity: %.3f\n",c)
     if !args["no_graph_dot"]
-        write_to_file(current_state,args["out_folder"],args["final_fname"])
+        write_to_file(current_state,out_fldr,args["final_fname"])
     end
     if args["plot_graphs"]
         plot(current_state)
     end
     if args["save_graphs_plot"]
-        plot_to_file(current_state,args["out_folder"],args["final_fname"])
+        plot_to_file(current_state,out_fldr,args["final_fname"])
     end
 
     if !args["no_chist"]
         PyPlot.plot(collect(1:tot_its),c_hist)
-        fname = joinpath(args["out_folder"],args["c_hist_fname"])
+        fname = joinpath(out_fldr,args["c_hist_fname"])
         savefig(fname)
     end
 end
